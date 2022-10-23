f059c4dd88b351b6847da252040a5736200ca929
Abel Garzón <abelgarzon@gmail.com>
2022-10-23 10:59:06 -0500

    python_inicial class 3 - condicionales

---
+++ ejercicios_profunidzacion/profundizacion_2.py
@@ -0,0 +1,83 @@
+# Condicionales [Python]
+# Ejercicios de profundización
+
+# Autor: Inove Coding School
+# Version: 3.0
+
+# NOTA:
+# Estos ejercicios son de mayor dificultad que los de clase y práctica.
+# Están pensados para aquellos con conocimientos previo o que dispongan
+# de mucho más tiempo para abordar estos temas por su cuenta.
+# Requiere mayor tiempo de dedicación e investigación autodidacta.
+
+# IMPORTANTE: NO borrar los comentarios en VERDE o NARANJA
+
+# Ejercicios de práctica con números
+'''
+Objetizo:
+Realizar un programa que solicite ingresar
+tres valores decimales de temperatura
+De las temperaturas ingresadas se determinará:
+1 - ¿Cuáles de ellas es la máxima temperatura ingresada?
+2 - ¿Cuáles de ellas es la mínima temperatura ingresada?
+3 - ¿Cuál es el promedio de las temperaturas ingresadas?
+
+IMPORTANTE: Para ordenar las temperatuas debe utilizar condicionales compuestos o anidados,
+no se busca utilizar bucles o algoritmos de ordenamiento ya que aún no hemos llegado a ese
+contenido. Recomendamos pensar bien este problema de lógica con un lápiz y papel.
+
+Alumno:
+- Deberá solicitar tres números decimales por consola,
+cada nuḿero de temperatura lo debe almacenar
+en variables llamadas:
+-> temperatura_1
+-> temperatura_2
+-> temperatura_3
+
+Luego, mediante el uso de condicionales, deberá determinar
+cuales de ellas es la mayor temperatura. Deberá almacenar
+el valor de la temperatura más alta en una nueva variable
+llamada:
+--> temperatura_max
+
+Luego, mediante el uso de condicionales, deberá determinar
+cuales de ellas es la menor temperatura. Deberá almacenar
+el valor de la temperatura más baja en una nueva variable
+llamada:
+--> temperatura_min
+
+- Al final imprimir en pantalla la variable temperatura_max
+  y temperatura_min
+'''
+
+print('Ejercicios de práctica con números')
+# Empezar aquí la resolución del ejercicio
+
+temperatura_1 = float(input('ingrese el valor del sensor 1:\n'))
+temperatura_2 = float(input('ingrese el valor del sensor 2:\n'))
+temperatura_3 = float(input('ingrese el valor del sensor 3:\n'))
+
+temperatura_max = 0
+temperatura_min = 0
+
+# calculamos temperatura promedio
+temperatura_promedio = (temperatura_1 + temperatura_2 + temperatura_3) / 3
+
+# identificanndo temperatura máxima
+if temperatura_1 >= temperatura_2 and temperatura_1 >= temperatura_3:
+  temperatura_max = temperatura_1
+elif temperatura_2 >= temperatura_1 and temperatura_2 >= temperatura_3:
+  temperatura_max = temperatura_2
+else:
+  temperatura_max = temperatura_3
+
+# identificanndo temperatura mínima
+if temperatura_1 <= temperatura_2 and temperatura_1 <= temperatura_3:
+  temperatura_min = temperatura_1
+elif temperatura_2 <= temperatura_1 and temperatura_2 <= temperatura_3:
+  temperatura_min = temperatura_2
+else:
+  temperatura_min = temperatura_3
+
+#Al finalizar, imprimir en pantalla las variables temperatura_max, temperatura_min y temperatura_promedio
+print(f'temperatura_max = {temperatura_max:.2f}\ntemperatura_min = {temperatura_min:.2f}\ntemperatura_promedio = {temperatura_promedio:.2f}')
